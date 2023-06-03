142e5b340a1677a4d0c1fcbf37acdcde430b7257
Danyal Ahmed <danyalsmail@gmail.com>
2023-06-03 09:53:59 -0400

    Create initial skeleton for tic-tac-toe board

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,23 @@
-import Image from 'next/image'
 
-export default function Home() {
+
+export default function Board() {
   return (
     <>
-    <h1>Test</h1>
+    <div className="flex flex-row items-stretch justify-evenly w-36 h-12 border border-collapse border-gray-950">
+      <button className="border border-black border-collapse flex-grow">X</button>
+      <button className="border border-black border-collapse flex-grow">X</button>
+      <button className="border border-black border-collapse flex-grow">X</button>
+    </div>
+    <div className="flex flex-row items-stretch  justify-evenly w-36 h-12 border border-collapse border-gray-950">
+      <button className="border border-black border-collapse flex-grow">X</button>
+      <button className="border border-black border-collapse flex-grow">X</button>
+      <button className="border border-black border-collapse flex-grow">X</button>
+    </div>
+    <div className="flex flex-row items-stretch  justify-evenly w-36 h-12 border border-collapse border-gray-950">
+      <button className="border border-black flex-grow">X</button>
+      <button className="border border-black flex-grow">X</button>
+      <button className="border border-black flex-grow">X</button>
+    </div>
     </>
   )
 }
