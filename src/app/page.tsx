b2e520ec88cd3ec09d2e09f5ed975f93182aadb0
Danyal <112341859+theonlynanu@users.noreply.github.com>
2023-06-03 15:24:59 -0400

    Externalize components
    This commit is a middle-of-the-road commit so that further changes
    can be done from another workstation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,22 @@
-
-
+import Square from "./square"
 export default function Board() {
   return (
-    <>
-    <div className="flex flex-row items-stretch justify-evenly w-36 h-12 border border-collapse border-gray-950">
-      <button className="border border-black border-collapse flex-grow">X</button>
-      <button className="border border-black border-collapse flex-grow">X</button>
-      <button className="border border-black border-collapse flex-grow">X</button>
+    <div className=" flex-row flex-wrap w-1/4 aspect-square">
+      <div className="flex flex-row items-stretch justify-evenly h-1/3">
+        <Square value="1"/>
+        <Square value="2"/>
+        <Square value="3"/>
+      </div>
+      <div className="flex flex-row items-stretch  justify-evenly h-1/3">
+        <Square value="4"/>
+        <Square value="5"/>
+        <Square value="6"/>
+      </div>
+      <div className="flex flex-row items-stretch  justify-evenly h-1/3">
+        <Square value="7"/>
+        <Square value="8"/>
+        <Square value="9"/>
+      </div>
     </div>
-    <div className="flex flex-row items-stretch  justify-evenly w-36 h-12 border border-collapse border-gray-950">
-      <button className="border border-black border-collapse flex-grow">X</button>
-      <button className="border border-black border-collapse flex-grow">X</button>
-      <button className="border border-black border-collapse flex-grow">X</button>
-    </div>
-    <div className="flex flex-row items-stretch  justify-evenly w-36 h-12 border border-collapse border-gray-950">
-      <button className="border border-black flex-grow">X</button>
-      <button className="border border-black flex-grow">X</button>
-      <button className="border border-black flex-grow">X</button>
-    </div>
-    </>
   )
 }
---
+++ src/app/square.tsx
@@ -0,0 +1,4 @@
+export default function Square({ value }: {value: string}) {
+    return <button className="border border-collapse border-black w-1/3 lg:text-6xl md:text-4xl flex-shrink">{value}</button>
+  }
+  
