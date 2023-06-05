0605bde963775a0f540dccf53f0d3749a30e2ac6
Danyal <112341859+theonlynanu@users.noreply.github.com>
2023-06-05 17:47:20 -0400

    Enable click handlers to update board array state

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,34 @@
+'use client'
+
 import Square from "./square"
+import { useState } from 'react';
+
 export default function Board() {
+  const [squares, setSquares] = useState<string[]|null[]>(Array(9).fill(null))
+
+  function handleClick(i: number) {
+    const nextSquares = squares.slice();
+    nextSquares[i] = "X";
+    setSquares(nextSquares);
+    console.log("clicked");
+  }
+
   return (
     <div className=" flex-row flex-wrap w-1/4 aspect-square">
       <div className="flex flex-row items-stretch justify-evenly h-1/3">
-        <Square value="1"/>
-        <Square value="2"/>
-        <Square value="3"/>
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
       </div>
       <div className="flex flex-row items-stretch  justify-evenly h-1/3">
-        <Square value="4"/>
-        <Square value="5"/>
-        <Square value="6"/>
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
       </div>
-      <div className="flex flex-row items-stretch  justify-evenly h-1/3">
-        <Square value="7"/>
-        <Square value="8"/>
-        <Square value="9"/>
+      <div className="flex flex-row items-stretch  justify-evenly h-1/3 ">
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
       </div>
     </div>
   )
--- src/app/square.tsx
+++ src/app/square.tsx
@@ -1,4 +1,17 @@
-export default function Square({ value }: {value: string}) {
-    return <button className="border border-collapse border-black w-1/3 lg:text-6xl md:text-4xl flex-shrink">{value}</button>
-  }
+'use client';
+import { useState } from 'react';
+
+export default function Square({ 
+    value, onSquareClick 
+}: {
+    value: string|null, onSquareClick():void
+}) {
+    return (
+    <button 
+        className="border border-collapse border-black w-1/3 lg:text-6xl md:text-4xl flex-shrink"
+        onClick={onSquareClick}
+    >
+        {value}
+    </button>
+  )}
   
