0ba9b985dfa61a7f079a072f6326f9d6af47f81b
Danyal <112341859+theonlynanu@users.noreply.github.com>
2023-06-21 00:17:48 -0400

    Remove `selectors` component
    Game mode selection to be handled in game component
    rather than complicating props for a fairly simple
    event handler

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
 import Game from "../components/game"
 import Board from "../components/board"
-import UI from "../components/selectors"
-
 
 export default function Index() {
 
--- src/components/selectors.tsx
+++
@@ -1,5 +0,0 @@
-export default function Selectors() {
-    return (
-       <>hi</> 
-    )
-}
