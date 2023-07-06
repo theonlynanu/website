6bc93b722955de8a59d0549ab56fefbe90eb58a1
Danyal <112341859+theonlynanu@users.noreply.github.com>
2023-07-06 18:10:13 -0400

    Update custom tailwind color palette in 'standard'

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,8 +12,23 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'russian-violet': '#0D0630',
-        'tiffany-blue': "#AFCCC2",
+        'standard': {
+          100: "#E8E6E1",
+          200: "#D3CEC4",
+          300: "#B8B2A7",
+          400: "#A39E93",
+          500: "#857F72",
+          600: "#625D52",
+          700: "#504A40",
+          800: "#423D33",
+          900: "#27241D",
+          primary: "#25D094",
+          darkprimary: "#2CDA9D",
+          confirm: "#008F56",
+          darkconfirm: "#00CC7A",
+          warn: "#E09600",
+          darkwarn: "#FFC247"
+        },
       }
     }
   }
