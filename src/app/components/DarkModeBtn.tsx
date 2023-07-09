138abbfab4c6e342cba4f7740989bcb4b1a13adc
Danyal <112341859+theonlynanu@users.noreply.github.com>
2023-07-09 17:56:48 -0400

    Removed onTap listener to fix mobile behavior

--- src/app/components/DarkModeBtn.tsx
+++ src/app/components/DarkModeBtn.tsx
@@ -42,7 +42,6 @@ const DarkModeBtn = () => {
             !isThemeLight ? "justify-end bg-standard-400": "justify-start bg-standard-200")} 
             onClick={toggleSwitch}
             whileHover="hover"
-            onTap={toggleSwitch}
             whileTap="tap"
             animate='rest'
         >
