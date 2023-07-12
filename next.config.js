624c01b691c0c4dd7d4883dae65d76ce45152ea9
Danyal Ahmed <danyalsmail@gmail.com>
2023-07-12 13:59:31 -0400

    Begin MDX configuration
    This commit was made mid-setup to facilitate
    switching between workstations. Don't expect any
    real changes here.

--- next.config.js
+++ next.config.js
@@ -8,5 +8,9 @@ const nextConfig = {
         API_KEY: process.env.API_KEY
     }
 }
+
+// Using MDX with remark or rehype plugins requires a touch more setup for this 
+// requirement, see Next docs for more info
 const withMDX = require('@next/mdx')()
+
 module.exports = withMDX(nextConfig)
