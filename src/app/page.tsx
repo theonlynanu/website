dee45d0db57829e1c9a1a2401f7084b4c34f3021
Danyal Ahmed <danyalsmail@gmail.com>
2023-07-17 12:36:10 -0400

    Create temp landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 import { motion } from 'framer-motion'
+import Link from 'next/link'
 // TODO - externalize components so that this page need not be a client component
 
 export default function Index() {
@@ -9,7 +10,12 @@ export default function Index() {
       {
         // TODO - Framer Motion components will need to be externalized
       }
-      <motion.img className='h-24 w-24' src='/profile.png' initial={{y:200}}whileInView={{y: 0}}/>
+      <div>This page is under construction, feel free to check out the preview branch of this project on my Github!</div>
+      <br />
+      <Link href='/tictactoe' className='rounded-full bg-standard-primary dark:bg-standard-darkprimary py-2 px-4 my-6 text-standard-100 dark:text-standard-900 hover:border-2 hover:border-standard-900 dark:hover:border-standard-100'>
+          Check out the tictactoe demo here!
+      </Link>
+      {/* <motion.img className='h-24 w-24' src='/profile.png' initial={{y:200}}whileInView={{y: 0}}/> */}
     </div>
   )
 }
