aa9ffea7f9176c90aa5fdf2e41e9cdfbdc492326
Danyal Ahmed <danyalsmail@gmail.com>
2023-07-17 17:37:44 -0400

    Remove unnecessary import & configure ssh signing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 'use client'
-import { motion } from 'framer-motion'
 import Link from 'next/link'
 // TODO - externalize components so that this page need not be a client component
 
@@ -13,7 +12,7 @@ export default function Index() {
       <div>This site is under construction, feel free to check out the preview branch of this project on my Github!</div>
       <br />
       <Link href='/tictactoe' className='rounded-full bg-standard-primary dark:bg-standard-darkprimary py-2 px-4 my-6 text-standard-900 hover:border-2 hover:border-standard-900 dark:hover:border-standard-100'>
-          Check out the tictactoe demo here!
+          Check out the tictactoe demo here! 
       </Link>
       {/* <motion.img className='h-24 w-24' src='/profile.png' initial={{y:200}}whileInView={{y: 0}}/> */}
     </div>
