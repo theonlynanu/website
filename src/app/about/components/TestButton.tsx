bad104edd65d49497360b07d1a0ea286a2dae987
Danyal Ahmed <danyalsmail@gmail.com>
2023-07-17 15:46:30 -0400

    Add temp landing page preview

---
+++ src/app/about/components/TestButton.tsx
@@ -0,0 +1,81 @@
+'use client'
+import { motion, useCycle } from "framer-motion"
+
+// TODO - Better handle props typing for additional reactive animation
+const HamLine = (props: any) => (
+    <motion.path 
+        fill='transparent'
+        stroke='#000'
+        strokeWidth='4'
+        strokeLinecap='round'
+        {...props}
+    />
+)
+
+
+export default function NavToggle() {
+const [isOpen, toggleOpen] = useCycle(true, false)
+
+
+    return (
+        <motion.div className="flex items-center shrink-0 top-2 mx-auto md:ml-8 w-16 h-16 z-10 bg-standard-primary dark:bg-standard-darkprimary rounded-full drop-shadow-lg cursor-pointer" 
+
+        whileTap={{scale: 0.9, transition:{duration: 0.2}}}
+        whileHover={{scale: [null, 1.05], transition:{duration:0.2}}}    
+        animate={isOpen ? 'open' : 'closed'}
+        variants={{open: {scale:0.8}}}  
+    >
+        <motion.svg
+            width='80'
+            height='65'
+            onClick={() => toggleOpen()}
+            viewBox='0 0 80 65'
+                initial='closed'
+                animate = {isOpen ? 'open' : 'closed'}
+            >
+            <HamLine 
+                variants={{
+                    closed: {
+                        opacity: 1,
+                        d: 'M28 22 L52 22',},
+                    open: {
+                        opacity: 1,
+                        d: 'M33 40 L47 25',
+                        strokeWidth: 3,
+                    }
+                }}
+                transition={{duration: 0.6}}
+            />
+            <HamLine 
+                variants={{
+                    closed: {
+                        opacity: 1,
+                        d: 'M28 32 L52 32'
+                    },
+                    open: {
+                        opacity: 0,
+                        x: -20
+                    }
+                }}
+                transition={{duration: 0.5}}
+                />
+            <HamLine 
+                variants={{
+                    closed: {
+                        opacity: 1,
+                        d: 'M28 42 L52 42'
+                    },
+                    open: {
+                        opacity: 1,
+                        d: 'M33 25 L47 40',
+                        strokeWidth: 3
+                    }
+                }}
+                transition={{duration: 0.6}}
+            />
+        </motion.svg>
+
+    </motion.div>
+    )
+}
+
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,9 +10,9 @@ export default function Index() {
       {
         // TODO - Framer Motion components will need to be externalized
       }
-      <div>This page is under construction, feel free to check out the preview branch of this project on my Github!</div>
+      <div>This site is under construction, feel free to check out the preview branch of this project on my Github!</div>
       <br />
-      <Link href='/tictactoe' className='rounded-full bg-standard-primary dark:bg-standard-darkprimary py-2 px-4 my-6 text-standard-100 dark:text-standard-900 hover:border-2 hover:border-standard-900 dark:hover:border-standard-100'>
+      <Link href='/tictactoe' className='rounded-full bg-standard-primary dark:bg-standard-darkprimary py-2 px-4 my-6 text-standard-900 hover:border-2 hover:border-standard-900 dark:hover:border-standard-100'>
           Check out the tictactoe demo here!
       </Link>
       {/* <motion.img className='h-24 w-24' src='/profile.png' initial={{y:200}}whileInView={{y: 0}}/> */}
