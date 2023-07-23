0225fa3ee1776a4946c2e028065dec5277b8ae77
Danyal <112341859+theonlynanu@users.noreply.github.com>
2023-07-23 18:15:18 -0400

    Add PopIn element to homepage button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import PopIn from './utils/popIn'
 
 export default function Index() {
 
@@ -9,12 +10,14 @@ export default function Index() {
       }
       <div>This site is under construction, feel free to check out the preview branch of this project on my Github!</div>
       <br />
+      <PopIn>
       <Link 
         href='/tictactoe' 
         className='rounded-full bg-standard-primary dark:bg-standard-darkprimary py-2 px-4 my-6 text-standard-900 hover:border-2 hover:border-standard-900 dark:hover:border-standard-100 drop-shadow-lg'
       >
           Check out the tictactoe demo here! 
       </Link>
+      </PopIn>
       {
         // TODO - Add link to palette page
       }
