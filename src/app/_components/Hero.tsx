481701161576743b6dca3881b351a6d5a8455c59
Danyal <112341859+theonlynanu@users.noreply.github.com>
2023-07-31 14:40:47 -0400

    Update github link to superproject

--- src/app/_components/Hero.tsx
+++ src/app/_components/Hero.tsx
@@ -20,9 +20,9 @@ function Box(props: MeshProps) {
       {...props}
       ref={meshRef}
       scale={active ? 1.5 : 1}
-      onClick={(event) => setActive(!active)}
-      onPointerOver={(event) => setHover(true)}
-      onPointerOut={(event) => setHover(false)}
+      onClick={() => setActive(!active)}
+      onPointerOver={() => setHover(true)}
+      onPointerOut={() => setHover(false)}
     >
       <boxGeometry args={[1, 1, 1]} />
       <meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
