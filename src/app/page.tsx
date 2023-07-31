138678d774bd9b594318df26129960f39c7069de
Danyal <112341859+theonlynanu@users.noreply.github.com>
2023-07-31 14:25:39 -0400

    Renew

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,42 +1,9 @@
-"use client";
-import Link from "next/link";
-import PopIn from "./_utils/popIn";
-import { useState, useRef } from "react";
-import { Canvas, useFrame } from "@react-three/fiber";
-
-function Box(props) {
-  // This reference will give us direct access to the mesh
-  const meshRef = useRef();
-  // Set up state for the hovered and active state
-  const [hovered, setHover] = useState(false);
-  const [ac tive, setActive] = useState(false);
-  // Subscribe this component to the render-loop, rotate the mesh every frame
-  useFrame((state, delta) => {
-    meshRef.current.rotation.x += delta;
-    meshRef.current.rotation.y += 0.5 * delta;
-  });
-  // Return view, these are regular three.js elements expressed in JSX
-  return (
-    <mesh
-      {...props}
-      ref={meshRef}
-      scale={active ? 1.5 : 1}
-      onClick={(event) => setActive(!active)}
-      onPointerOver={(event) => setHover(true)}
-      onPointerOut={(event) => setHover(false)}
-    >
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
-    </mesh>
-  );
-}
+import Hero from "./_components/Hero";
 
 export default function Index() {
   return (
-    <Canvas className="border border-black rounded-full bg-standard-900">
-      <ambientLight />
-      <pointLight position={[10, 10, 10]} />
-      <Box position={[0, 0, 2.5]} />
-    </Canvas>
+    <div className="h-1/2">
+      <Hero />
+    </div>
   );
 }
