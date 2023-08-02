915e7116fe57d477762e6164516dcf3b098c03e2
Danyal <112341859+theonlynanu@users.noreply.github.com>
2023-08-02 15:02:41 -0400

    Create basic skeleton for Hero
    Interstitial commit for changing workstations

--- src/app/_components/Hero.tsx
+++ src/app/_components/Hero.tsx
@@ -1,9 +1,11 @@
 "use client";
 import { useState, useRef } from "react";
-import { Canvas, MeshProps, ThreeElements, useFrame } from "@react-three/fiber";
+import { Canvas, MeshProps, useFrame } from "@react-three/fiber";
 import { Mesh } from "three";
+import { Flex, Box, useFlexSize } from "@react-three/flex";
+import { Float, OrbitControls } from "@react-three/drei";
 
-function Box(props: MeshProps) {
+function Cube(props: MeshProps) {
   // This reference will give us direct access to the mesh
   const meshRef = useRef<Mesh>(null!);
   // Set up state for the hovered and active state
@@ -11,31 +13,53 @@ function Box(props: MeshProps) {
   const [active, setActive] = useState(false);
   // Subscribe this component to the render-loop, rotate the mesh every frame
   useFrame((state, delta) => {
-    meshRef.current.rotation.x += delta;
-    meshRef.current.rotation.y += 0.5 * delta;
+    meshRef.current.rotation.y += delta;
   });
   // Return view, these are regular three.js elements expressed in JSX
   return (
     <mesh
       {...props}
       ref={meshRef}
-      scale={active ? 1.5 : 1}
       onClick={() => setActive(!active)}
       onPointerOver={() => setHover(true)}
       onPointerOut={() => setHover(false)}
+      rotation={[0.7, 0.3, 0.7]}
     >
       <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
+      <meshStandardMaterial color={hovered ? "#D3CEC4" : "#857F72"} />
     </mesh>
+  );
+}
+
+function Cone(props: MeshProps) {
+  const meshRef = useRef<Mesh>(null!);
+  return (
+    <Float floatingRange={[-0.5, 1]} rotationIntensity={3} speed={3}>
+      <mesh {...props} ref={meshRef} rotation={[0.3, -2, 0.4]}>
+        <coneGeometry args={[1.5, 4]} />
+        <meshStandardMaterial color={"green"} />
+      </mesh>
+    </Float>
   );
 }
 
 export default function Hero() {
   return (
-    <Canvas className="h-full">
-      <ambientLight />
-      <pointLight position={[10, 10, 10]} />
-      <Box position={[0, 0, 2.5]} />
-    </Canvas>
+    <div className="flex flex-shrink flex-col mx-auto justify-center gap-4 ">
+      <div className="w-fit">
+        <Canvas className="border ">
+          <ambientLight intensity={0.2} />
+          <pointLight position={[0, 4, 3]} />
+          <Cube scale={3} />
+        </Canvas>
+      </div>
+      <div className="self-end">
+        <Canvas className="border ">
+          <ambientLight intensity={0.4} />
+          <pointLight position={[0, 2, 2]} />
+          <Cone />
+        </Canvas>
+      </div>
+    </div>
   );
 }
