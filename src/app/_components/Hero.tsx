8ae36c712c4a98ce8e4dc0a820edb7f5c96ca59c
Danyal <112341859+theonlynanu@users.noreply.github.com>
2023-08-08 13:52:55 -0400

    Update splash page/hero banner

--- src/app/_components/Hero.tsx
+++ src/app/_components/Hero.tsx
@@ -1,10 +1,11 @@
 "use client";
 import { useState, useRef } from "react";
 import { Canvas, MeshProps, useFrame } from "@react-three/fiber";
-import { Mesh } from "three";
+import { Mesh, Vector3 } from "three";
 import { Float } from "@react-three/drei";
 import useWindowDimensions from "../_utils/useWindowDimension";
 import { TypeAnimation } from "react-type-animation";
+import { inter, poiret } from "../../fonts";
 
 function Cube(props: MeshProps) {
   // This reference will give us direct access to the mesh
@@ -18,11 +19,11 @@ function Cube(props: MeshProps) {
     if (!active) {
       meshRef.current.rotation.y += 0.5 * delta;
       meshRef.current.position.x += delta;
-      meshRef.current.position.y += 0.4 * delta;
+      meshRef.current.position.y += 0.2 * delta;
     } else {
       meshRef.current.rotation.y -= 0.5 * delta;
       meshRef.current.position.x -= delta;
-      meshRef.current.position.y -= 0.4 * delta;
+      meshRef.current.position.y -= 0.2 * delta;
     }
   });
 
@@ -44,20 +45,61 @@ function Cube(props: MeshProps) {
 
 function Loop(props: MeshProps) {
   const meshRef = useRef<Mesh>(null!);
+  const [tubeSegments, setTubeSegments] = useState(32);
+  const [hovered, setHovered] = useState(false);
 
   return (
     <>
       <mesh {...props}>
         <Float floatingRange={[-0.5, 0.3]} rotationIntensity={6} speed={2}>
-          <mesh rotation={[0, 2, 0]}>
-            <torusGeometry args={[1.5, 0.6]} />
-            <meshStandardMaterial color={"#857F72"} />
+          <mesh
+            ref={meshRef}
+            rotation={[0, 2, 0]}
+            onClick={() => {
+              tubeSegments == 32
+                ? setTubeSegments(Math.max(Math.random() * 12, 4))
+                : setTubeSegments(32);
+            }}
+            onPointerEnter={() => setHovered(true)}
+            onPointerLeave={() => setHovered(false)}
+          >
+            <torusGeometry args={[1.5, 0.6, 12, tubeSegments]} />
+            <meshStandardMaterial color={hovered ? "#A39E93" : "#857F72"} />
           </mesh>
         </Float>
-        <mesh ref={meshRef}>
+        <mesh>
           <sphereGeometry args={[0.5]} />
           <meshStandardMaterial color={"#CC8800"} />
         </mesh>
+      </mesh>
+    </>
+  );
+}
+
+function Cone(props: MeshProps) {
+  const meshRef = useRef<Mesh>(null!);
+  const [radialSegments, setRadialSegments] = useState(8);
+  const [hovered, setHovered] = useState(false);
+
+  const axis = new Vector3(0, 1, 0);
+  useFrame((state, delta) => {
+    meshRef.current.rotateOnAxis(axis, delta);
+  });
+
+  return (
+    <>
+      <mesh
+        {...props}
+        rotation={[0.8, 0.4, -0.3]}
+        ref={meshRef}
+        onClick={() =>
+          radialSegments < 32 ? setRadialSegments(radialSegments + 1) : null
+        }
+        onPointerEnter={() => setHovered(true)}
+        onPointerLeave={() => setHovered(false)}
+      >
+        <coneGeometry args={[1, 3, radialSegments]} />
+        <meshStandardMaterial color={hovered ? "#FFC247" : "#CC8800"} />
       </mesh>
     </>
   );
@@ -86,9 +128,15 @@ export default function Hero() {
           position={[width ? width / 200 : 2, height ? height / 5000 : -1, 0]}
           scale={1}
         />
+        <Cone
+          position={[width ? -width / 200 : -2, height ? height / 175 : 4, -8]}
+          scale={width ? Math.max(width / 1250, 0.8) : 1.4}
+        />
       </Canvas>
       <span className="block mx-auto my-auto absolute top-0 md:top-32 left-0 right-0 w-1/2 text-center rounded-xl">
-        <div className="[text-shadow:_0_0px_2px_var(--tw-shadow-color)] text-6xl w-full m-8 text-center mx-auto text-standard-900 shadow-white font-['georgia']">
+        <div
+          className={`[text-shadow:_0_0px_2px_var(--tw-shadow-color)] text-6xl w-full m-8 text-center mx-auto text-standard-900 shadow-white ${inter.className} font-medium`}
+        >
           Hi, I'm Danyal
         </div>
       </span>
