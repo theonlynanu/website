737bf56ce998ade9a0be28cc7ad241910c70ee4b
Danyal <112341859+theonlynanu@users.noreply.github.com>
2023-08-15 01:44:16 -0400

    Remove flat shading from eyeball in Hero banner

--- src/app/_components/HeroSphere.tsx
+++ src/app/_components/HeroSphere.tsx
@@ -14,12 +14,12 @@ export default function HeroSphere(props: MeshProps) {
 
   return (
     <mesh {...props} ref={meshRef}>
-      <meshStandardMaterial color={"white"} flatShading />
-      <sphereGeometry args={[0.8, 12, 12]} />
-      <Text fontSize={2} position-y={-0.3} position-z={0.81} color={"black"}>
+      <meshStandardMaterial color={"white"} />
+      <sphereGeometry args={[0.8, 18, 18]} />
+      <Text fontSize={2} position-y={-0.3} position-z={0.8} color={"black"}>
         •
       </Text>
-      <Text fontSize={3} position-y={-0.4} position-z={0.8} color={"#CC8800"}>
+      <Text fontSize={3} position-y={-0.4} position-z={0.79} color={"#CC8800"}>
         •
       </Text>
     </mesh>
