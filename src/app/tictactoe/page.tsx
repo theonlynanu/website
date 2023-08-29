4b65e83f5aaff7d4d7b837cb7b1b69ffdc343fbb
Danyal <112341859+theonlynanu@users.noreply.github.com>
2023-08-29 17:40:49 -0400

    Add message to game concerning gcloud cold starts

--- src/app/tictactoe/page.tsx
+++ src/app/tictactoe/page.tsx
@@ -9,6 +9,12 @@ export default function Index() {
   return (
     <section className="mb-12">
       <Game />
+      <p className="px-8 pt-6">
+        If you're making a move against the AI, it may take a little bit for it
+        to play its first move. This is due to cold-starts with the Google Cloud
+        Run server, the opponent's first move should be played within 30
+        seconds. If you have persistent issues, please contact me!
+      </p>
     </section>
   );
 }
