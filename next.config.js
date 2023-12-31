08d43855f9e850f149a35826bd9b6ccef8c2f147
Danyal <112341859+theonlynanu@users.noreply.github.com>
2023-12-30 23:59:25 -0500

    Update packages and add .mdx files to next-config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
   experimental: {
     mdxRs: true,
   },
