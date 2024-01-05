13cdf9f30cbe5bfba24b6fbd3522e270d0501ba9
Danyal <112341859+theonlynanu@users.noreply.github.com>
2024-01-05 17:21:33 -0500

    Fix typo in className

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -36,7 +36,7 @@ const galleryItems: galleryItem[] = [
 export default function Index() {
   return (
     <div className="">
-      <h1 className={`mx-g8 my-4 text-3xl ${montaga.className}`}>Gallery</h1>
+      <h1 className={`mx-8 my-4 text-3xl ${montaga.className}`}>Gallery</h1>
       <p className="mx-8 text-sm md:text-base">
         A collection of small tools or projects. If you have any ideas,
         suggestions, or bugs to report, feel free to shoot me a message via my
